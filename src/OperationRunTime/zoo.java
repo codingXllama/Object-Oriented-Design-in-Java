d975548f8146691c20f4a6f44721c5bbcf33f992
Osama Al-Chalabi <osamaalchalabi2@gmail.com>
2019-06-27 23:17:40 -0600

    Added a quiz 2 solution

--- src/OperationRunTime/zoo.java
+++ src/OperationRunTime/zoo.java
@@ -7,10 +7,14 @@ public class zoo {
     {
         Animal animal1= new Animal(12,"MALE",221);
         animal1.eat();
+        //** Note ** animal1.fly(); will give us an error, because Animal class does NOT contain the fly method since it's only in the Bird class
         //animal1.sleep();
 
-        Bird bird1= new Bird();
+        Bird bird1= new Bird(31,"Female",319);
         bird1.Fly();
+        // the bird can access the sleep method, because the Bird class extends from the Animal class (sub class of the Animal class)
+        //this allows it to use the Super classes methods, and instance variables.
+        bird1.sleep();
 
 
 
