bddb12a15ea528179d02c10705e9fda454e7fb9b
Osama Al-Chalabi <osamaalchalabi2@gmail.com>
2019-06-27 22:57:02 -0600

    made the fish class into a subclass of the Animal super class

--- src/OperationRunTime/Bird.java
+++ src/OperationRunTime/Bird.java
@@ -3,6 +3,7 @@ package OperationRunTime;
 //The subclass 'Bird' extends the super class 'Animal'
 public class Bird extends Animal {
 
+    //A bird is an Animal and it needs the same attributes (instances) as the animal class, hence we must create the same constructor
     //creating the Bird constructor that is the same as the Animal class constructor
     //we have to create this constructor or else we will get an error, because the Animal class contains that constructor and the sub class does not
     public Bird(int age, String gender, int weightInLbs)
--- src/OperationRunTime/Fish.java
+++ src/OperationRunTime/Fish.java
@@ -1,4 +1,17 @@
 package OperationRunTime;
 
-public class Fish {
+public class Fish extends  Animal{
+
+    //creating a Fish constructor that matches the inherits from the Animal class
+    //We do this, because the Fish is an animal. So what ever values the animal contains then the fish must also have that.
+    public Fish(int age, String gender, int weightInLbs)
+    {
+        super(age,gender,weightInLbs);
+    }
+    //creating a method for the Fish class
+    public void Swim()
+    {
+        System.out.println("I'am swimming");
+    }
+
 }
