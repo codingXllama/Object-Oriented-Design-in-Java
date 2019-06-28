87b480cd445cca57754a66ca77788a9899a6ddb2
Osama Al-Chalabi <osamaalchalabi2@gmail.com>
2019-06-27 22:52:08 -0600

    Creating a super and sub class

--- src/OperationRunTime/Bird.java
+++ src/OperationRunTime/Bird.java
@@ -1,6 +1,19 @@
 package OperationRunTime;
 
-public class Bird {
+//The subclass 'Bird' extends the super class 'Animal'
+public class Bird extends Animal {
+
+    //creating the Bird constructor that is the same as the Animal class constructor
+    //we have to create this constructor or else we will get an error, because the Animal class contains that constructor and the sub class does not
+    public Bird(int age, String gender, int weightInLbs)
+    {
+        //super() allows us to use the instance variables directly from the super class which is the Animal
+        super(age, gender, weightInLbs);
+    }
+    {
+
+    }
+
     public void Fly()
     {
         System.out.println("I'am flying...");
